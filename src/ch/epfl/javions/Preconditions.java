1edc2d083f23dbc504a26f30223293afbee48ec0
JitterKiller <a1337player@gmail.com>
2023-02-21 09:47:08 +0100

    first class created

--- src/ch/epfl/javions/Preconditions.java
+++ src/ch/epfl/javions/Preconditions.java
@@ -1,5 +1,12 @@
 package ch.epfl.javions;
 
-public class Preconditions {
+public final class Preconditions {
 
+    private Preconditions() {
+    }
+    public static  void checkArgument(boolean shouldBeTrue){
+        if(!shouldBeTrue){
+            throw new IllegalArgumentException();
+        }
+    }
 }
