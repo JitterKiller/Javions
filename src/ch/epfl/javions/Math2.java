08c1ad7b119306dd2e9aa8675b419c820ce78a6c
JitterKiller <a1337player@gmail.com>
2023-02-21 10:01:00 +0100

    Debut de la classe Math2

---
+++ src/ch/epfl/javions/Math2.java
@@ -0,0 +1,24 @@
+package ch.epfl.javions;
+
+public final class Math2 {
+
+    private Math2(){}
+
+    public static int clamp(int min, int v, int max){
+        if(min <= max) {
+            if(v < min) {
+                return min;
+            } else if (v > max) {
+                return max;
+            } else {
+                return v;
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static double asinh(double x){
+        return Math.log(x+Math.sqrt(1+Math.pow(x,2)));
+    }
+}
