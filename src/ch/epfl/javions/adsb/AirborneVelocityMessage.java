1d124c2c59b530f495a8f5f8c9d72921c6c89d2b
JitterKiller <a1337player@gmail.com>
2023-03-23 22:06:52 +0100

    Step 6

--- src/ch/epfl/javions/adsb/AirborneVelocityMessage.java
+++ src/ch/epfl/javions/adsb/AirborneVelocityMessage.java
@@ -17,42 +17,54 @@ public record AirborneVelocityMessage(long timeStampNs, IcaoAddress icaoAddress,
 
     public static AirborneVelocityMessage of(RawMessage rawMessage) {
 
-        int subtype = Bits.extractUInt(rawMessage.payload(), 48, 3);
-        if (subtype != 1 && subtype != 2 && subtype != 3 && subtype != 4) {return null;}
+        int subType = Bits.extractUInt(rawMessage.payload(), 48, 3);
+        if (!(subType >= 1 && subType <= 4)) {
+            return null;
+        }
 
         double angle;
         double velocity;
 
 
-        if (subtype == 1 || subtype == 2){
+        if (subType == 1 || subType == 2){
 
             int Dew = Bits.extractUInt(rawMessage.payload(), 21, 1);
             int Vew = Bits.extractUInt(rawMessage.payload(), 11, 10);
             int Dns = Bits.extractUInt(rawMessage.payload(), 10, 1);
             int Vns = Bits.extractUInt(rawMessage.payload(), 0, 10);
 
-            if (Vns == 0 || Vew == 0) {return null;}
+            if (Vns == 0 || Vew == 0) {
+                return null;
+            }
 
             velocity = Math.hypot(Vew - 1, Vns - 1);
             double defaultAngle = Math.atan2(Vns,Vew);
 
             if (Dew == 0) {
-                if (Dns == 0) {angle = Math.PI/2 - defaultAngle;} else {angle = Math.PI/2 + defaultAngle;}
+                if (Dns == 0) {
+                    angle = Math.PI/2 - defaultAngle;
+                } else {
+                    angle = Math.PI/2 + defaultAngle;
+                }
             } else {
-                if (Dns == 0) {angle = Math.PI *(3/2) + defaultAngle;} else {angle = Math.PI *(3/2) - defaultAngle;}
+                if (Dns == 0) {
+                    angle = Math.PI * (3/2) + defaultAngle;
+                } else {
+                    angle = Math.PI * (3/2) - defaultAngle;
+                }
             }
 
         } else {
 
             int SH = Bits.extractUInt(rawMessage.payload(), 21, 1);
             if (SH == 1) {
-                int HDG = Bits.extractUInt(rawMessage.payload(), 11, 10);
-                angle = Units.convertFrom(HDG/Math.scalb(1, 10), Units.Angle.TURN);
+                byte HDG = (byte) Bits.extractUInt(rawMessage.payload(), 11, 10);
+                angle = Units.convertFrom(Math.scalb(Byte.toUnsignedInt(HDG),- 10), Units.Angle.TURN);
                 velocity = Bits.extractUInt(rawMessage.payload(), 0, 10);
             } else {return null;}
         }
 
-        if (subtype == 1 || subtype == 3) {
+        if (subType == 1 || subType == 3) {
             angle = Units.convert(angle, Units.Speed.KNOT, (Units.Time.HOUR/Units.KILO) * Units.Speed.KILOMETER_PER_HOUR);
         } else {
             angle = Units.convert(angle, 4 * Units.Speed.KNOT, (Units.Time.HOUR/Units.KILO) * Units.Speed.KILOMETER_PER_HOUR);
